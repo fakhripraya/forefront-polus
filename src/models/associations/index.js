988505721c743275f960116415be2a0a812ce258
fakhripraya <fakhripraya.p@gmail.com>
2023-11-18 01:23:19 +0700

    fix association

---
+++ src/models/associations/index.js
@@ -0,0 +1,186 @@
+const {
+  MasterCategory,
+} = require("../objects/master_category");
+const { MasterFile } = require("../objects/master_file");
+const {
+  MasterStoreCatalogue,
+} = require("../objects/master_stores_catalogue");
+const {
+  MasterStoreChannels,
+} = require("../objects/master_stores_channels");
+const {
+  MasterStoreEmployees,
+} = require("../objects/master_stores_employees");
+const { MasterStore } = require("../objects/master_stores");
+const { MasterUser } = require("../user/master_user");
+const {
+  MasterStoreDisplayItem,
+} = require("../objects/master_stores_display_item");
+
+const InitAssociations = () => {
+  // MASTER_USER - MASTER_STORE //
+  MasterStore.belongsTo(MasterUser, {
+    foreignKey: {
+      name: "userId",
+      allowNull: false,
+    },
+    targetKey: "id",
+    constraints: false,
+  });
+  MasterUser.hasMany(MasterStore, {
+    foreignKey: {
+      name: "userId",
+      allowNull: false,
+    },
+    sourceKey: "id",
+    constraints: false,
+  });
+
+  // MASTER_USER - MASTER_STORE_EMPLOYEES //
+  MasterStoreEmployees.belongsTo(MasterUser, {
+    foreignKey: {
+      name: "userId",
+      allowNull: false,
+    },
+    targetKey: "id",
+    constraints: false,
+  });
+  MasterUser.hasMany(MasterStoreEmployees, {
+    foreignKey: {
+      name: "userId",
+      allowNull: false,
+    },
+    sourceKey: "id",
+    constraints: false,
+  });
+
+  // MASTER_STORE - MASTER_STORE_EMPLOYEES //
+  MasterStoreEmployees.belongsTo(MasterStore, {
+    foreignKey: {
+      name: "storeId",
+      allowNull: false,
+    },
+    targetKey: "id",
+    constraints: false,
+  });
+  MasterStore.hasMany(MasterStoreEmployees, {
+    foreignKey: {
+      name: "storeId",
+      allowNull: false,
+    },
+    sourceKey: "id",
+    constraints: false,
+  });
+
+  // MASTER_STORE - MASTER_STORE_CATALOGUE //
+  MasterStoreCatalogue.belongsTo(MasterStore, {
+    foreignKey: {
+      name: "storeId",
+      allowNull: false,
+    },
+    targetKey: "id",
+    constraints: false,
+  });
+  MasterStore.hasMany(MasterStoreCatalogue, {
+    foreignKey: {
+      name: "storeId",
+      allowNull: false,
+    },
+    sourceKey: "id",
+    constraints: false,
+  });
+
+  // MASTER_STORE_DISPLAY_ITEM - MASTER_STORE_CATALOGUE //
+  MasterStoreDisplayItem.belongsTo(MasterStoreCatalogue, {
+    foreignKey: {
+      name: "catalogueId",
+      allowNull: false,
+    },
+    targetKey: "id",
+    constraints: false,
+  });
+  MasterStoreCatalogue.hasMany(MasterStoreDisplayItem, {
+    foreignKey: {
+      name: "catalogueId",
+      allowNull: false,
+    },
+    sourceKey: "id",
+    constraints: false,
+  });
+
+  // MASTER_STORE_DISPLAY_ITEM - MASTER_CATEGORY //
+  MasterStoreDisplayItem.belongsTo(MasterCategory, {
+    foreignKey: {
+      name: "categoryId",
+      allowNull: false,
+    },
+    targetKey: "id",
+    constraints: false,
+  });
+  MasterCategory.hasMany(MasterStoreDisplayItem, {
+    foreignKey: {
+      name: "categoryId",
+      allowNull: false,
+    },
+    sourceKey: "id",
+    constraints: false,
+  });
+
+  // MASTER_STORE - MASTER_FILE //
+  MasterFile.belongsTo(MasterStore, {
+    foreignKey: {
+      name: "storeId",
+      allowNull: true,
+    },
+    targetKey: "id",
+    constraints: false,
+  });
+  MasterStore.hasMany(MasterFile, {
+    foreignKey: {
+      name: "storeId",
+      allowNull: true,
+    },
+    sourceKey: "id",
+    constraints: false,
+  });
+
+  // MASTER_STORE_DISPLAY_ITEM - MASTER_FILE //
+  MasterFile.belongsTo(MasterStoreDisplayItem, {
+    foreignKey: {
+      name: "displayItemId",
+      allowNull: true,
+    },
+    targetKey: "id",
+    constraints: false,
+  });
+  MasterStoreDisplayItem.hasMany(MasterFile, {
+    foreignKey: {
+      name: "displayItemId",
+      allowNull: true,
+    },
+    sourceKey: "id",
+    constraints: false,
+  });
+
+  // MASTER_STORE - MASTER_STORE_CHANNELS //
+  MasterStoreChannels.belongsTo(MasterStore, {
+    foreignKey: {
+      name: "storeId",
+      allowNull: false,
+    },
+    targetKey: "id",
+    constraints: false,
+  });
+  MasterStore.hasMany(MasterStoreChannels, {
+    foreignKey: {
+      name: "storeId",
+      allowNull: false,
+    },
+    sourceKey: "id",
+    constraints: false,
+  });
+};
+
+module.exports = {
+  InitAssociations,
+};
--- src/models/index.js
+++ src/models/index.js
@@ -1,29 +1,5 @@
 const { db } = require("../config");
 const {
-  MasterStoreAssociations,
-} = require("../associations/master_store");
-const {
-  MasterStoreCatalogueAssociations,
-} = require("../associations/master_store_catalogue");
-const {
-  MasterStoreChannelsAssociations,
-} = require("../associations/master_store_channels");
-const {
-  MasterStoreDisplayItemAssociations,
-} = require("../associations/master_store_display_item");
-const {
-  MasterStoreEmployeesAssociations,
-} = require("../associations/master_store_employees");
-const {
-  MasterUserAssociations,
-} = require("../associations/master_user");
-const {
-  MasterCategoryAssociations,
-} = require("../associations/master_category");
-const {
-  MasterFileAssociations,
-} = require("../associations/master_file");
-const {
   MASTER_COURIER_SEED,
 } = require("../seeds/master_courier");
 const {
@@ -36,19 +12,10 @@ const {
   MasterCategory,
 } = require("../models/objects/master_category");
 const { SequelizeRollback } = require("../utils/functions");
+const { InitAssociations } = require("./associations");
 
 const InitModels = async () => {
-  // init associations
-  MasterCategoryAssociations();
-  MasterFileAssociations();
-  MasterStoreCatalogueAssociations();
-  MasterStoreChannelsAssociations();
-  MasterStoreDisplayItemAssociations();
-  MasterStoreEmployeesAssociations();
-  MasterStoreAssociations();
-  MasterUserAssociations();
-
-  // sync db
+  InitAssociations();
   await db
     .sync({ alter: true, force: false })
     .then(() => {
