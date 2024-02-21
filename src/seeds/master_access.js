e849d9fa02c0aab8def7a78e77a578bbb8abaa5b
fakhripraya <fakhripraya.p@gmail.com>
2024-02-21 15:35:54 +0700

    add more access

--- src/seeds/master_access.js
+++ src/seeds/master_access.js
@@ -8,20 +8,44 @@ const {
   ADD_STORE,
   VIEW_CHANNEL,
   SEND_MESSAGES,
+  ABSOLUTE,
+  STORE_OWNER,
+  STORE_ADMINISTRATOR,
 } = require("../variables/accesses");
 const {
   ACTIVE,
   GENERAL_ACCESS,
   CREATIVE_STORE_ACCESS,
   DASHBOARD_ACCESS,
+  STORE_GENERAL_ACCESS,
 } = require("../variables/general");
 
 const MASTER_ACCESS_SEED = [
+  // GENERAL_ACCESS
+  {
+    accessType: GENERAL_ACCESS,
+    accessName: ABSOLUTE,
+    status: ACTIVE,
+  },
   {
     accessType: GENERAL_ACCESS,
     accessName: ADMINISTRATOR,
     status: ACTIVE,
   },
+
+  // STORE_GENERAL_ACCESS
+  {
+    accessType: STORE_GENERAL_ACCESS,
+    accessName: STORE_OWNER,
+    status: ACTIVE,
+  },
+  {
+    accessType: STORE_GENERAL_ACCESS,
+    accessName: STORE_ADMINISTRATOR,
+    status: ACTIVE,
+  },
+
+  // DASHBOARD_ACCESS
   {
     accessType: DASHBOARD_ACCESS,
     accessName: VIEW_HOME,
@@ -52,6 +76,8 @@ const MASTER_ACCESS_SEED = [
     accessName: ADD_STORE,
     status: ACTIVE,
   },
+
+  // CREATIVE_STORE_ACCESS
   {
     accessType: CREATIVE_STORE_ACCESS,
     accessName: VIEW_CHANNEL,
